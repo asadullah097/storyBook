52ad1b6890c1182dbd19cf047f2847f5d9070e00
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2021-04-17 16:05:47 +0500

    final commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 
 const dotenv=require('dotenv');
 
+const methodOverride = require('method-override')
+
 const passport=require('passport');
 
 const session = require('express-session')
@@ -23,19 +25,36 @@ dotenv.config({ path:'./config/config.env'});
 const connectDB=require('./config/db');
 
 const app=express();
+
 //body parser
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+// Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
 connectDB();
 //logging
 if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'))
 }
 //Handlebar helper
-const {formatDate,truncate,stripTags}=require('./helpers/helper');
+const {
+    formatDate,
+    truncate,
+    stripTags,
+    editIcon,
+    select,
+}
+=require('./helpers/helper');
 //handlebar
 app.engine('.hbs',exphbs({ helpers: {
-    formatDate,truncate,stripTags}, defaultLayout:'main',extname:'.hbs'}));
+    formatDate,truncate,stripTags,editIcon,select}, defaultLayout:'main',extname:'.hbs'}));
 
 app.set('view engine', '.hbs');
 //session
@@ -52,6 +71,11 @@ app.use(
 app.use(passport.initialize());
 
 app.use(passport.session());
+//global
+app.use(function(req,res,next){
+  res.locals.user = req.user || null
+  next()
+});
 //static folder
 app.use(express.static(path.join(__dirname,'public')));
 
