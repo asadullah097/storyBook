458e956326007630640c91d89f7d632b9390ac58
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2021-05-30 09:22:36 +0500

    .gitignore file added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const connectDB= async ()=>{
     try {
-        const conn=await mongoose.connect(process.env.MONGO_URI,{
+        const conn=await mongoose.connect(process.env.DB_CONNECT,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
             useFindAndModify:true
